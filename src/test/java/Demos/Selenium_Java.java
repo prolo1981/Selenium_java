3f9579652ca683aa2d2a85dd9287d1d3d6c599f9
SebastianProlo <prolosebastian4@gmail.com>
2022-10-14 17:16:30 -0300

    Initial commit

---
+++ src/test/java/Demos/Selenium_Java.java
@@ -0,0 +1,34 @@
+package Demos;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Selenium_Java {
+
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://jqueryui.com/droppable/");
+        System.out.println(driver.getTitle());
+        Actions ac = new Actions(driver);
+        WebElement w = driver.findElement(By.xpath("//iframe[@class='demo-frame']"));
+        driver.switchTo().frame(w);
+
+        ac.dragAndDrop(driver.findElement(By.xpath("//div[@id='draggable']")),driver.findElement(By.xpath("//div[@id='droppable']"))).build().perform();
+
+
+
+
+
+
+    }
+}
+
